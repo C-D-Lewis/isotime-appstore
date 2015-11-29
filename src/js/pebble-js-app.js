0dab84257d8426a603111a240b49f7e1fa44e3fc
Chris Lewis <Bonsitm@gmail.com>
2015-11-28 23:15:52 -0800

    Bump version, begin JS

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,7 @@
+var VERSION = '1.5';
+
+/******************************** PebbleKit JS ********************************/
+
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready! Version: ' + VERSION);
+});
