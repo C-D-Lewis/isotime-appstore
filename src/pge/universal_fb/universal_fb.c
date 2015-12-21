b41767ba0a891329e17b74582165b7a53a7e8fad
Chris Lewis <Bonsitm@gmail.com>
2015-12-21 12:35:16 +0000

    Update universal-fb

--- src/pge/universal_fb/universal_fb.c
+++ src/pge/universal_fb/universal_fb.c
@@ -6,22 +6,6 @@
  
 #include "universal_fb.h"
 
-/******************************* Compatibility ********************************/
-
-// Provide GBitmapDataRowInfo API on 2.x
-#if defined(PBL_SDK_2)
-GBitmapDataRowInfo gbitmap_get_data_row_info(GBitmap *bitmap, int y) {
-  uint8_t *ptr = gbitmap_get_data(bitmap);
-  int rsb = gbitmap_get_bytes_per_row(bitmap);
-  ptr = &ptr[y * rsb];
-  return (GBitmapDataRowInfo) {
-    .min_x = 0, .max_x = 144, .data = ptr // Start of this row
-  };
-}
-#endif
-
-/********************************** Internal **********************************/
-
 static bool byte_get_bit(uint8_t *byte, uint8_t bit) {
   return ((*byte) >> bit) & 1;
 }
@@ -34,12 +18,12 @@ static void byte_set_bit(uint8_t *byte, uint8_t bit, uint8_t value) {
 
 GColor universal_fb_get_pixel_color(GBitmapDataRowInfo info, GRect bounds, GPoint point) {
   if(point.x > info.min_x && point.x < info.max_x
-  && point.y > 0 && point.y < bounds.size.h) {
+  && point.y > bounds.origin.y && point.y < bounds.origin.y + bounds.size.h) {
 #if defined(PBL_COLOR)
     return (GColor){ .argb = info.data[point.x] };
 #elif defined(PBL_BW)
     uint8_t byte = point.x / 8;
-    uint8_t bit = point.x % 8; // fb: bwbb bbbb -> byte: 0000 0010
+    uint8_t bit = point.x % 8; 
     return byte_get_bit(&info.data[byte], bit) ? GColorWhite : GColorBlack;
 #endif
   } else {
@@ -50,13 +34,13 @@ GColor universal_fb_get_pixel_color(GBitmapDataRowInfo info, GRect bounds, GPoin
 
 void universal_fb_set_pixel_color(GBitmapDataRowInfo info, GRect bounds, GPoint point, GColor color) {
   if(point.x > info.min_x && point.x < info.max_x
-  && point.y > 0 && point.y < bounds.size.h) {
+  && point.y > bounds.origin.y && point.y < bounds.origin.y + bounds.size.h) {
 #if defined(PBL_COLOR)
     memset(&info.data[point.x], color.argb, 1);
 #elif defined(PBL_BW)
     uint8_t byte = point.x / 8;
-    uint8_t bit = point.x % 8; // fb: bwbb bbbb -> byte: 0000 0010
-    byte_set_bit(&info.data[byte], bit, color.argb);
+    uint8_t bit = point.x % 8; 
+    byte_set_bit(&info.data[byte], bit, gcolor_equal(color, GColorWhite) ? 1 : 0);
 #endif
   } else {
     // Out of bounds
@@ -68,12 +52,11 @@ void universal_fb_swap_colors(GBitmap *fb, GRect bounds, GColor c1, GColor c2) {
   for(int y = bounds.origin.y; y < bounds.origin.y + bounds.size.h; y++) {
     GBitmapDataRowInfo info = gbitmap_get_data_row_info(fb, y);
     for(int x = bounds.origin.x; x < bounds.origin.x + bounds.size.w; x++) {
-      if(gcolor_equal(universal_fb_get_pixel_color(info, bounds, GPoint(x, y)), c1)) {
-        // Replace c1 with c2
-        universal_fb_set_pixel_color(info, bounds, GPoint(x, y), c2);
-      } else if(gcolor_equal(universal_fb_get_pixel_color(info, bounds, GPoint(x, y)), c2)) {
-        // Vice versa
-        universal_fb_set_pixel_color(info, bounds, GPoint(x, y), c1);
+      GPoint p = GPoint(x, y);
+      if(gcolor_equal(universal_fb_get_pixel_color(info, bounds, p), c1)) {
+        universal_fb_set_pixel_color(info, bounds, p, c2);
+      } else if(gcolor_equal(universal_fb_get_pixel_color(info, bounds, p), c2)) {
+        universal_fb_set_pixel_color(info, bounds, p, c1);
       }
     }
   }
